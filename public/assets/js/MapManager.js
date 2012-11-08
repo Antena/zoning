f6082cb36735f804c8894b17a13f39e6f979ac71
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2012-11-08 19:06:40 -0300

    Magic

--- public/assets/js/MapManager.js
+++ public/assets/js/MapManager.js
@@ -10,72 +10,42 @@ var MapManager = function MapManager(options) {
     };
     this.map = new google.maps.Map(document.getElementById(options.containerDivId), googleMapOptions);
     this.ftClient = new FTClient(options.ftId);
+    this.ftLayer = new google.maps.FusionTablesLayer();
+    this.ftId = options.ftId;
 
-    // Added townships
-    var townships = [];
+    this.townships = {};
 
     this.getMap = function() {
         return this.map;
     }
 
-    this.addTownship = function(name, options) {
-        var self = this;
-        // query ft
-        self.ftClient.query(["Id", "Nombre", "N", "E", "S", "W"], "Nombre = '" + name + "'", function(data) {
-            var rows = data.table.rows;
-            var map = self.getMap();
-
-            for (var i=0; i<rows.length; i++) {
-                var township = rows[i];
-                var id = township[0];
-                var name = township[1];
-                var imagesUrlHead = self._buildImagesUrlHead(name);
-
-                // Fusion Tables  township limit on map
-                new google.maps.FusionTablesLayer({
-                    query: {
-                        select: self.options.ftLimitColumnName,
-                        from: self.options.ftId
-                    },
-                    map: map
-                });
-
-                // Overlay coordinates
-                var neCoordinate = new google.maps.LatLng(township[2], township[3]);
-                var swCoordinate = new google.maps.LatLng(township[4], township[5]);
-                var latLngBounds = new google.maps.LatLngBounds(swCoordinate, neCoordinate);
-
-                // Overlay: urbArea t0
-                var urbArea = new google.maps.GroundOverlay(
-                    imagesUrlHead + self.options.urbAreaFilename + "_t0.png",
-                    latLngBounds,
-                    { opacity: 0.5 }
-                );
-                urbArea.setMap(map);
-
-                // Overlay: urbFootprint t0
-                var urbFootprint = new google.maps.GroundOverlay(
-                    imagesUrlHead + self.options.urbFootprintFilename + "_t0.png",
-                    latLngBounds,
-                    { opacity: 0.5 }
-                );
-                urbFootprint.setMap(map);
-
-                // Overlay: urbFootprint t0
-                var newDevelopment = new google.maps.GroundOverlay(
-                    imagesUrlHead + self.options.newDevelopmentFilename + ".png",
-                    latLngBounds,
-                    { opacity: 0.5 }
-                );
-                newDevelopment.setMap(map);
-            }
-        })
+    this.getTownships = function() {
+        return this.townships;
     }
 
-    // UTILITY FUNCTIONS
-    this._buildImagesUrlHead = function(name) {
-        var head = "/assets/images/townships/";
-        head += name.toLowerCase().replace(" ","_") + "/";
-        return head;
+    this.addTownship = function(name, success) {
+        var self = this;
+
+        self.ftClient.query(["Id", "Nombre", "N", "E", "S", "W"], "Nombre = '" + name + "'", function(data) {
+
+            if (data.table.rows.length > 1) {
+                throw "ERROR: Query returned more than 1 township";
+            }
+
+            var row = data.table.rows[0];
+            var township = new Township({
+                id : row[0],
+                name : row[1],
+                n : row[2],
+                e : row[3],
+                s : row[4],
+                w : row[5],
+                mapManager: self
+            });
+            township.init();
+            self.townships[name] = township;
+
+            success.call(township);
+        })
     }
 }
---
+++ public/assets/js/Township.js
@@ -0,0 +1,78 @@
+var Township = function Township(options) {
+
+    this.options = options;
+    this.mapManager = options.mapManager;
+
+    this.map = options.mapManager.getMap()
+    this.ftLimitColumnName = options.mapManager.options.ftLimitColumnName;
+    this.ftId = options.mapManager.ftId;
+
+    this.urbArea_t0 = null;
+    this.urbArea_t1 = null;
+    this.urbFootprint_t0 = null;
+    this.urbFootprint_t1 = null;
+    this.newDevelopment = null;
+
+    this.urbAreaFilename = "urbArea";
+    this.urbFootprintFilename = "urbFootprint";
+    this.newDevelopmentFilenmae = "newDevelopment";
+
+    var neCoordinate = new google.maps.LatLng(options.n, options.e);
+    var swCoordinate = new google.maps.LatLng(options.s, options.w);
+    this.latLngBounds = new google.maps.LatLngBounds(swCoordinate, neCoordinate);
+
+    this.init = function() {
+        var self = this;
+
+        // Overlay : urbArea t0
+        this.urbArea_t0 = new google.maps.GroundOverlay(
+            this._buildImagesUrlHead(options.name) + this.urbAreaFilename + "_t0.png",
+            this.latLngBounds,
+            { opacity: 0.5 }
+        );
+
+        // Overlay : urbFootprint t0
+        this.urbFootprint_t0 = new google.maps.GroundOverlay(
+            this._buildImagesUrlHead(options.name) + this.urbFootprintFilename + "_t0.png",
+            this.latLngBounds,
+            { opacity: 0.5 }
+        );
+
+        // Overlay : newDevelopment
+        this.newDevelopment = new google.maps.GroundOverlay(
+            this._buildImagesUrlHead(options.name) + this.newDevelopmentFilenmae + ".png",
+            this.latLngBounds,
+            { opacity: 0.5 }
+        );
+    }
+
+    this.showLimit = function() {
+        var self = this;
+        self.mapManager.ftLayer.setOptions({
+            query: {
+                select: self.ftLimitColumnName,
+                from: self.ftId
+            },
+            map: self.map
+        });
+    }
+
+    this.showUrbAreaT0 = function() {
+        this.urbArea_t0.setMap(this.map);
+    }
+
+    this.showUrbFootprintT0 = function() {
+        this.urbFootprint_t0.setMap(this.map);
+    }
+
+    this.showNewDevelopment = function() {
+        this.newDevelopment.setMap(this.map);
+    }
+
+    // UTILITY METHODS
+    this._buildImagesUrlHead = function(name) {
+        var head = "/assets/images/townships/";
+        head += name.toLowerCase().replace(" ","_") + "/";
+        return head;
+    }
+}
