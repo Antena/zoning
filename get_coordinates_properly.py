3c2afaa61a6b6241df15034f57e7f3890a997ebf
dnul <diego@antena.co>
2012-12-03 01:07:36 -0300

    Added proper coordinates script

---
+++ get_coordinates_properly.py
@@ -0,0 +1,40 @@
+from osgeo import osr, gdal
+
+# get the existing coordinate system
+ds = gdal.Open('urbArea_t0.img')
+old_cs= osr.SpatialReference()
+old_cs.ImportFromWkt(ds.GetProjectionRef())
+
+# create the new coordinate system
+wgs84_wkt = """
+GEOGCS["WGS 84",
+    DATUM["WGS_1984",
+        SPHEROID["WGS 84",6378137,298.257223563,
+            AUTHORITY["EPSG","7030"]],
+        AUTHORITY["EPSG","6326"]],
+    PRIMEM["Greenwich",0,
+        AUTHORITY["EPSG","8901"]],
+    UNIT["degree",0.01745329251994328,
+        AUTHORITY["EPSG","9122"]],
+    AUTHORITY["EPSG","4326"]]"""
+new_cs = osr.SpatialReference()
+new_cs .ImportFromWkt(wgs84_wkt)
+
+# create a transform object to convert between coordinate systems
+transform = osr.CoordinateTransformation(old_cs,new_cs) 
+
+#get the point to transform, pixel (0,0) in this case
+width = ds.RasterXSize
+height = ds.RasterYSize
+gt = ds.GetGeoTransform()
+minx = gt[0]
+miny = gt[3] + width*gt[4] + height*gt[5] 
+maxx = gt[0] + width*gt[1] + height*gt[2]
+maxy = gt[3]
+#get the coordinates in lat long
+(north,west,a) = transform.TransformPoint(minx,miny) 
+(south,east,a) = transform.TransformPoint(maxx,maxy)
+print north
+print west
+print south
+print east
